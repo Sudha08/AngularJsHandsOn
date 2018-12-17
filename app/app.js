b76c8123800567dbf32c06a3e6a36373b5c7b912
SUDHA CHANDRAMOULEESWARAN <963209@TCS.com>
2018-12-17 15:07:58 +0530

    committed code for transclude & replace properties

--- app/app.js
+++ app/app.js
@@ -24,6 +24,8 @@ myApp.directive('randomAvenger', [function(){
             name: '='
         },
         templateUrl: 'views/random.html',
+        transclude: true,
+        replace: true,
         controller: function($scope) {
             $scope.random = Math.floor(Math.random() * 4);
         }
